1132a270b4a1714b3eb2f1265738bee51b97bdf9
Demys52 <demysnunes@hotmail.com>
2021-12-11 12:53:23 -0300

    meu primeiro commit

---
+++ routes/api.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/', function (Request $request) {
+    try {
+        //* LOG FATURAMENTO
+        $logFaturamento = Log::channel("sucesso");
+        $logFaturamento->warning('REQUISIÇÃO DO RASTREADOR', [
+            'Request' => [
+                $request->all()
+            ],
+        ]);
+        return response()->json(['pagina correta enviando mensagem para o slack'], 200);
+    } catch (\Exception $e) {
+        response()->json([$e->getMessage()], 400);
+    }
+});
+Route::fallback(function (Request $request){
+    try {
+        //* LOG FATURAMENTO
+        $logFaturamento = Log::channel("error-404");
+        $logFaturamento->warning('REQUISIÇÃO DO RASTREADOR', [
+            'Request' => [
+                $request->all()
+            ],
+        ]);
+        return response()->json(['pagina não encontrada, enviando mensagem para o slack'], 400);
+    } catch (\Exception $e) {
+        response()->json([$e->getMessage()], 400);
+    }
+});
---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function (Request $request) {
+    try {
+        //* LOG FATURAMENTO
+        $logFaturamento = Log::channel("sucesso");
+        $logFaturamento->warning('REQUISIÇÃO DO RASTREADOR', [
+            'Request' => [
+                $request->all()
+            ],
+        ]);
+        return response()->json(['pagina correta enviando mensagem para o slack'], 200);
+    } catch (\Exception $e) {
+        response()->json([$e->getMessage()], 400);
+    }
+});
+Route::fallback(function (Request $request){
+    try {
+        //* LOG FATURAMENTO
+        $logFaturamento = Log::channel("error-404");
+        $logFaturamento->warning('REQUISIÇÃO DO RASTREADOR', [
+            'Request' => [
+                $request->all()
+            ],
+        ]);
+        return response()->json(['pagina não encontrada, enviando mensagem para o slack'], 400);
+    } catch (\Exception $e) {
+        response()->json([$e->getMessage()], 400);
+    }
+});
